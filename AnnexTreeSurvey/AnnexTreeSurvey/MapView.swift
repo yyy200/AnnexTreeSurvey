717e13775b7f3cef6c5f1d422caff9fd0da58d45
Youssef El Mays <youssefelmays@Youssef's-MacBook-pro.local>
2022-04-17 19:56:46 -0400

    CODE

--- AnnexTreeSurvey/AnnexTreeSurvey/MapView.swift
+++ AnnexTreeSurvey/AnnexTreeSurvey/MapView.swift
@@ -16,45 +16,45 @@ struct MapView: View {
     
     let locationManager = CLLocationManager()
 
-    func getRoute() {
-        guard let url = URL(string: "http://0.0.0.0:3000/route") else { return }
-        let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
-            if let error = error {
-              print("Error accessing server: \(error)")
-              return
-            }
-            guard let httpResponse = response as? HTTPURLResponse,
-                       (200...299).contains(httpResponse.statusCode) else {
-                print("Error with the response, unexpected status code: \(String(describing: response))")
-               return
-            }
-            if let data = data {
-                    guard let geojson = try? MKGeoJSONDecoder().decode(data) else { fatalError("wrong format") }
-                    var overlays = [MKOverlay]()
-                    
-                    for item in geojson {
-                        if let feature = item as? MKGeoJSONFeature {
-                            for geo in feature.geometry {
-                                if let multistring = geo as? MKMultiPolyline {
-                                    overlays.append(multistring)
-                                }
-                            }
-                        }
-                    }
-                    
-                    self.objs = overlays
-            }
-        })
-        
-        task.resume()
-    }
+//    func getRoute() {
+//        guard let url = URL(string: "http://0.0.0.0:3000/route") else { return }
+//        let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+//            if let error = error {
+//              print("Error accessing server: \(error)")
+//              return
+//            }
+//            guard let httpResponse = response as? HTTPURLResponse,
+//                       (200...299).contains(httpResponse.statusCode) else {
+//                print("Error with the response, unexpected status code: \(String(describing: response))")
+//               return
+//            }
+//            if let data = data {
+//                    guard let geojson = try? MKGeoJSONDecoder().decode(data) else { fatalError("wrong format") }
+//                    var overlays = [MKOverlay]()
+//
+//                    for item in geojson {
+//                        if let feature = item as? MKGeoJSONFeature {
+//                            for geo in feature.geometry {
+//                                if let multistring = geo as? MKMultiPolyline {
+//                                    overlays.append(multistring)
+//                                }
+//                            }
+//                        }
+//                    }
+//
+//                    self.objs = overlays
+//            }
+//        })
+//
+//        task.resume()
+//    }
     
     var body: some View {
         ZStack(alignment: .bottom) {
-            Map(coordinateRegion: $viewModel.region, showsUserLocation: true).accentColor(Color.mint).frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 0, idealHeight: .infinity, maxHeight: .infinity, alignment: .bottom).ignoresSafeArea().onAppear(perform: viewModel.checkForLocationServices)
+            TheMAP().accentColor(Color.mint).frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 0, idealHeight: .infinity, maxHeight: .infinity, alignment: .bottom).ignoresSafeArea()
             
             NavigationLink("Log A Tree", destination: TreeLogger()).customStyle()
-        }.onAppear(perform: getRoute)
+        }
     }
 }
 
@@ -104,4 +104,73 @@ final class MapViewModel : NSObject, ObservableObject, CLLocationManagerDelegate
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         checkLocationAuth()
     }
+}
+
+import Contacts
+
+struct TheMAP: UIViewRepresentable {
+    
+  typealias UIViewType = MKMapView
+    
+    @StateObject var viewModel = MapViewModel()
+    
+     
+     func makeCoordinator() -> MapViewCoordinator {
+       return MapViewCoordinator()
+     }
+     
+    func makeUIView(context: Context) -> MKMapView {
+       let mapView = MKMapView()
+         viewModel.checkForLocationServices()
+         viewModel.locationManagerDidChangeAuthorization(viewModel.locationManager!)
+         mapView.delegate = context.coordinator
+         mapView.setRegion(viewModel.region, animated: true)
+         mapView.showsUserLocation = true
+         mapView.setUserTrackingMode(.follow, animated: true)
+        
+         let p1 = MKPlacemark(coordinate: viewModel.region.center, addressDictionary: [CNPostalAddressCountryKey:"Start"])
+         let p2 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 43.67194329249824, longitude: -79.4074375128927), addressDictionary: [CNPostalAddressCountryKey:"Tree"])
+         let p3 = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 43.67103484529716, longitude: -79.4030050400584), addressDictionary: [CNPostalAddressCountryKey:"Tree"])
+
+         let firstLeg = MKDirections.Request()
+         let secondLeg = MKDirections.Request()
+         
+         firstLeg.source = MKMapItem(placemark:p1)
+         firstLeg.destination = MKMapItem(placemark: p2)
+         firstLeg.transportType = .walking
+         
+         secondLeg.source = MKMapItem(placemark: p2)
+         secondLeg.destination = MKMapItem(placemark: p3)
+         secondLeg.transportType = .walking
+         
+    
+        let fldirections = MKDirections(request: firstLeg)
+        let sldirections = MKDirections(request: secondLeg)
+        fldirections.calculate { response, error in
+            guard let route1 = response?.routes.first else { return }
+            sldirections.calculate { res, err in
+                guard let route2 = res?.routes.first else { print("an error \(String(describing: err))");return }
+
+                mapView.addOverlays([route1.polyline, route2.polyline])
+            }
+        }
+        
+        mapView.addAnnotations([p2, p3])
+        
+        return mapView
+     }
+  
+
+  func updateUIView(_ uiView: MKMapView, context: Context) {
+      uiView.setRegion(viewModel.region, animated: true)
+  }
+    
+    class MapViewCoordinator: NSObject, MKMapViewDelegate {
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+          let renderer = MKPolylineRenderer(overlay: overlay)
+          renderer.strokeColor = .systemBlue
+          renderer.lineWidth = 5
+          return renderer
+        }
+      }
 }
